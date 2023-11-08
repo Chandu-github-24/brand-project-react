711d23b08c616af0e4a2474558fe44c1d2cf43c9
Chandu S <chandusl2411999@gmail.com>
2023-11-08 12:24:05 +0530

    brandproject

---
+++ brand_project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Navigation from "./component/Navigation"
+import HeroSection from "./component/Hero"
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <HeroSection />
+    </>
+  )
+}
+
+
+export default App
---
+++ brand_project/src/component/Navigation.jsx
@@ -0,0 +1,27 @@
+const Navigation = () => {
+
+    return (
+        <>
+            <div>
+                <nav className="container">
+
+                    <div className="logo">
+                        <img src="images/brand_logo.png" alt="nikelogo" />
+                    </div>
+
+                    <ul>
+                        <li href="#">MENU</li>
+                        <li href="#">LOCATION</li>
+                        <li href="#">ABOUT</li>
+                        <li href="#">CONTACT</li>
+                    </ul>
+
+                    <button>login</button>
+
+                </nav>
+            </div>
+        </>
+    )
+
+}
+export default Navigation;
